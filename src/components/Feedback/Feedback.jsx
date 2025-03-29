6693591696d1472907ff6a7be2eb2d659db7c5d3
Ivane <ivanmitj@gmail.com>
2025-03-29 14:55:19 +0100

    FIX

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -4,11 +4,11 @@ import styles from './Feedback.module.css';
 const Feedback = ({ feedback, totalFeedback, positiveFeedback }) => {
   return (
     <div className={styles.feedback}>
-      <p>Total feedback: {totalFeedback}</p>
       <p className="positive">Good: {feedback.good}</p>
       <p className="neutral">Neutral: {feedback.neutral}</p>
       <p className="bad">Bad: {feedback.bad}</p>
-      <p>Positive feedback: {positiveFeedback}%</p>
+      <p>Total: {totalFeedback}</p>
+      <p>Positive: {positiveFeedback}%</p>
     </div>
   );
 };
